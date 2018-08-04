70ab71c663db22c96b6f0c52c01fa023483c9674
Teus Benschop <teusjannette@gmail.com>
2018-08-04 18:04:41 +0200

    C++ website generator skeleton

---
+++ generator.cpp
@@ -0,0 +1,25 @@
+
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <exception>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <istream>
+#include <limits>
+#include <memory>
+#include <ostream>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+
+
+int main (int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
